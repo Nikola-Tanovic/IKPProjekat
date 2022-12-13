5a7401e618cec7c01808c57e757bde5d15b260bc
VidakGrujic <vidakspartanacgrujic@gmail.com>
2022-12-13 16:37:55 +0100

    implemented listen thread function as different thread in main function.

--- IKP_Projekat/WinSockServer/Server.cpp
+++ IKP_Projekat/WinSockServer/Server.cpp
@@ -10,93 +10,38 @@
 
 bool InitializeWindowsSockets();
 
-typedef struct threadParameters {
+typedef struct clientCommunicationThreadParameters {
     SOCKET acceptedSocket;
     sockaddr_in clientAddr;
-    CRITICAL_SECTION cs;
+    CRITICAL_SECTION hashMapCS;
+    CRITICAL_SECTION threadListCS;
+    CRITICAL_SECTION printCS;
     DWORD threadId;
-    DWORD* returnValue;
-}threadParameters;
+    int* exitFlag;
+    threadNode** head;
+}clientCommunicationThreadParameters;
 
-DWORD WINAPI clientThreadFunction(LPVOID lpParam) {
+typedef struct listenThreadParameters {
+    SOCKET listenSocket;
+    SOCKET acceptedSocket;
+    threadNode** head;
+    CRITICAL_SECTION hashMapCS;
+    CRITICAL_SECTION threadListCS;
+    CRITICAL_SECTION printCS;
+    int* exitFlag;
+    DWORD threadId;
+}listenThreadParameters;
 
-    char recvBuff[DEFAULT_BUFLEN];
-    unsigned long mode = 1;
-
-    threadParameters tParameters = *(threadParameters*)lpParam;
-
-    int iResult = ioctlsocket(tParameters.acceptedSocket, FIONBIO, &mode);
-    if (iResult != NO_ERROR)
-        printf("ioctlsocket failed with error: %ld\n", iResult);
-
-    fd_set readfds;
-
-    FD_ZERO(&readfds);
-
-    FD_SET(tParameters.acceptedSocket, &readfds);
-
-    timeval timeVal;
-    timeVal.tv_sec = 1;
-    timeVal.tv_usec = 0;
-
-    printf("Konektovan novi klijent\n");
-
-    do
-    {
-        FD_ZERO(&readfds);
-        FD_SET(tParameters.acceptedSocket, &readfds);
-
-        int result = select(0, &readfds, NULL, NULL, &timeVal);
-
-        if (result == 0) {
-            //printf("Vreme za cekanje je isteklo (select pre recviver funkcije)\n");
-            //Sleep(1000);
-            continue;
-        }
-        else if (result == SOCKET_ERROR) {
-            // connection was closed gracefully
-            printf("Connection with client closed(bababab).\n");
-            closesocket(tParameters.acceptedSocket);
-            break;
-        }
-
-        // Receive data until the client shuts down the connection
-        iResult = recv(tParameters.acceptedSocket, recvBuff, DEFAULT_BUFLEN, 0);
-        if (iResult > 0)
-        {
-            EnterCriticalSection(&(tParameters.cs));
-            printf("Client: IPAddress: %s Port: %d\n", inet_ntoa(tParameters.clientAddr.sin_addr), ntohs(tParameters.clientAddr.sin_port));
-            printf("Thread ID: %d\n", tParameters.threadId);
-            printf("Message received from client: %s.\n", recvBuff);
-            FD_CLR(tParameters.acceptedSocket, &readfds);
-            LeaveCriticalSection(&(tParameters.cs));
-        }
-        else if (iResult == 0)
-        {
-            // connection was closed gracefully
-            //da li je ovde klijent gracefully zatvorio i kada se ulazi u ovaj deo koda
-            printf("Connection with client closed.\n");
-            closesocket(tParameters.acceptedSocket);
-        }
-        else
-        {
-            // there was an error during recv
-            printf("recv failed with error: %d\n", WSAGetLastError());
-            closesocket(tParameters.acceptedSocket);
-        }
-    } while (1);
-
-    return 0;
-    // here is where server shutdown loguc could be placed
-}
-
+DWORD WINAPI clientThreadFunction(LPVOID lpParam);
+DWORD WINAPI listenThreadFunction(LPVOID lpParam);
 
 int  main(void)
 {
     // Socket used for listening for new clients 
     SOCKET listenSocket = INVALID_SOCKET;
     // Socket used for communication with client
-    SOCKET acceptedSocket = INVALID_SOCKET;
+    //SOCKET acceptedSocket = INVALID_SOCKET;
+    
     // variable used to store function return value
     int iResult;
     // Buffer used for storing incoming data
@@ -104,15 +49,19 @@ int  main(void)
 
     unsigned long mode = 1;
     int result = 0;
+    int exitFlag = 0;
 
     //creating and initializing critical section 
-    CRITICAL_SECTION cs;
-    InitializeCriticalSection(&cs);
+    CRITICAL_SECTION printCS;
+    InitializeCriticalSection(&printCS);
 
     //critical section that controls access to threadList
     CRITICAL_SECTION threadListCS;
     InitializeCriticalSection(&threadListCS);
 
+    //critical section that controls hash map access
+    CRITICAL_SECTION hashMapCS;
+    InitializeCriticalSection(&hashMapCS);
 
     //initializing list of threads
     threadNode* head = NULL;
@@ -188,13 +137,159 @@ int  main(void)
     if (iResult != NO_ERROR)
         printf("ioctlsocket failed with error: %ld\n", iResult);
 
+    
+    /// <summary>
+    /// pocetak thread-a gde se poziva accept funckija
+    /// </summary>
+    /// <param name=""></param>
+    /// <returns></returns>
+    
+    listenThreadParameters* ltParams = (listenThreadParameters*)malloc(sizeof(listenThreadParameters));
+    ltParams->listenSocket = listenSocket;
+    ltParams->head = &head;
+    ltParams->hashMapCS = hashMapCS;
+    ltParams->threadListCS = threadListCS;
+    ltParams->printCS = printCS;
+    ltParams->exitFlag = &exitFlag;
+    ltParams->threadId = 0;
+    ltParams->acceptedSocket = INVALID_SOCKET;
+
+    // listenSocket, head, hashMapCS, threadListCS, printCS
+
+    HANDLE listenThread = CreateThread(NULL, 0, &listenThreadFunction, ltParams, 0, &(ltParams->threadId));
+    
+    EnterCriticalSection(&threadListCS);
+    printf("Thread ID(unutar main funkcije servera, ID listen thread-a): %d\n", ltParams->threadId);
+    threadNode* tn = createNewThreadNode(listenThread, ltParams->threadId);
+    insertAtHead(&head, tn);
+    printList(head);
+    printf("Listen thread pokrenut, unesite 'exit' za kraj\n");
+    LeaveCriticalSection(&threadListCS);
+
+    int c = getchar();
+
+    //promenjiva koja detektuje exit na mainu se prosledjuje lisent thread-u a on prosledjuje thradoivma za komunikaciju sa klijentima
+    //taj gethchar stoji u main threadu posle listen thread-a
+    //
+
+    // shutdown the connection since we're done
+    /*iResult = shutdown(acceptedSocket, SD_SEND);
+    if (iResult == SOCKET_ERROR)
+    {
+        printf("shutdown failed with error: %d\n", WSAGetLastError());
+        closesocket(acceptedSocket);
+        WSACleanup();
+        return 1;
+    }
+
+    // cleanup*/
+
+    closesocket(listenSocket);
+    
+    //closesocket(acceptedSocket);
+
+    WSACleanup();
+
+    //zatvaramo threadove
+
+    return 0;
+}
+
+bool InitializeWindowsSockets()
+{
+    WSADATA wsaData;
+    // Initialize windows sockets library for this process
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
+        return false;
+    }
+    return true;
+}
+
+DWORD WINAPI clientThreadFunction(LPVOID lpParam) {
+
+    char recvBuff[DEFAULT_BUFLEN];
+    unsigned long mode = 1;
+
+    clientCommunicationThreadParameters* tParameters = (clientCommunicationThreadParameters*)lpParam;
+
+    int iResult = ioctlsocket(tParameters->acceptedSocket, FIONBIO, &mode);
+    if (iResult != NO_ERROR)
+        printf("ioctlsocket failed with error: %ld\n", iResult);
+
     fd_set readfds;
+
+    FD_ZERO(&readfds);
+
+    FD_SET(tParameters->acceptedSocket, &readfds);
+
+    timeval timeVal;
+    timeVal.tv_sec = 1;
+    timeVal.tv_usec = 0;
+
+    //printf("Konektovan novi klijent\n");
+
+    do
+    {
+        FD_ZERO(&readfds);
+        FD_SET(tParameters->acceptedSocket, &readfds);
+
+        int result = select(0, &readfds, NULL, NULL, &timeVal);
+
+        if (result == 0) {
+            //printf("Vreme za cekanje je isteklo (select pre recviver funkcije)\n");
+            //Sleep(1000);
+            continue;
+        }
+        else if (result == SOCKET_ERROR) {
+            // connection was closed gracefully
+            printf("Connection with client closed(bababab).\n");
+            closesocket(tParameters->acceptedSocket);
+            break;
+        }
+
+        // Receive data until the client shuts down the connection
+        iResult = recv(tParameters->acceptedSocket, recvBuff, DEFAULT_BUFLEN, 0);
+        if (iResult > 0)
+        {
+            EnterCriticalSection(&(tParameters->printCS));
+            printf("Client: IPAddress: %s Port: %d\n", inet_ntoa(tParameters->clientAddr.sin_addr), ntohs(tParameters->clientAddr.sin_port));
+            printf("Thread ID: %d\n", tParameters->threadId);
+            printf("Message received from client: %s.\n", recvBuff);
+            FD_CLR(tParameters->acceptedSocket, &readfds);
+            LeaveCriticalSection(&(tParameters->printCS));
+        }
+        else if (iResult == 0)
+        {
+            // connection was closed gracefully
+            //da li je ovde klijent gracefully zatvorio i kada se ulazi u ovaj deo koda
+            printf("Connection with client closed.\n");
+            closesocket(tParameters->acceptedSocket);
+        }
+        else
+        {
+            // there was an error during recv
+            printf("recv failed with error: %d\n", WSAGetLastError());
+            closesocket(tParameters->acceptedSocket);
+        }
+    } while (1);
+    //zatvaranje thread-a i close socket 
+    return 0;
+    // here is where server shutdown loguc could be placed
+}
+
+DWORD WINAPI listenThreadFunction(LPVOID lpParam) {
+    
+    listenThreadParameters* ltParams = (listenThreadParameters*)lpParam;
+    fd_set readfds;
+    int result = -1;
 
     do
     {
         FD_ZERO(&readfds);
 
-        FD_SET(listenSocket, &readfds);
+        FD_SET(ltParams->listenSocket, &readfds);
 
         timeval timeVal;
         timeVal.tv_sec = 1;
@@ -210,24 +305,24 @@ int  main(void)
         else if (result == SOCKET_ERROR) {
             // connection was closed gracefully
             printf("Connection with client closed.\n");
-            closesocket(acceptedSocket);
+            closesocket(ltParams->acceptedSocket);
             break;
         }
-        else if (FD_ISSET(listenSocket, &readfds)) {
+        else if (FD_ISSET(ltParams->listenSocket, &readfds)) {
             // Wait for clients and accept client connections.
             // Returning value is acceptedSocket used for further
             // Client<->Server communication. This version of
             // server will handle only one client.
-       
+
             sockaddr_in clientAddr;
             int clientAddrSize = sizeof(struct sockaddr_in);
 
-            acceptedSocket = accept(listenSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
+            ltParams->acceptedSocket = accept(ltParams->listenSocket, (struct sockaddr*)&clientAddr, &clientAddrSize);
 
-            if (acceptedSocket == INVALID_SOCKET)
+            if (ltParams->acceptedSocket == INVALID_SOCKET)
             {
                 printf("accept failed with error: %d\n", WSAGetLastError());
-                closesocket(listenSocket);
+                closesocket(ltParams->listenSocket);
                 WSACleanup();
                 return 1;
             }
@@ -237,27 +332,28 @@ int  main(void)
 
             //cuvamo povratnu vrednost niti
             DWORD threadReturnValue = -1;
-            
-           
-            threadParameters tParameters;
-            tParameters.acceptedSocket = acceptedSocket;
-            tParameters.clientAddr = clientAddr;
-            tParameters.cs = cs;
-            tParameters.threadId = 0;
-            tParameters.returnValue = &threadReturnValue;
 
-            HANDLE thread = CreateThread(NULL, 0, &clientThreadFunction, &tParameters, 0, &(tParameters.threadId));
-           
+
+            clientCommunicationThreadParameters* tParameters = (clientCommunicationThreadParameters*)malloc(sizeof(clientCommunicationThreadParameters)); //ovde alocirati memoriju mallocom.
+            tParameters->acceptedSocket = ltParams->acceptedSocket;
+            tParameters->clientAddr = clientAddr;
+            tParameters->printCS = ltParams->printCS;
+            tParameters->hashMapCS = ltParams->hashMapCS;
+            tParameters->threadListCS = ltParams->threadListCS;
+            tParameters->threadId = 0;
+            tParameters->exitFlag = ltParams->exitFlag;     //umesto return value zvacemo ga exit koji cemo upisivati u main threa-du
+            tParameters->head = ltParams->head;             //dodacemo i polje head pokazivac na listu thread=ova koji kominiciraju sa klijentom.
+
+            HANDLE clientCommunicaitonThread = CreateThread(NULL, 0, &clientThreadFunction, tParameters, 0, &(tParameters->threadId));
+
             //postaviti pitanje vezano za ovo
             //da li moze da se desi da se ne napuni tParameters.threadId a da se udje u ovu kriticnu sekciju?
-            EnterCriticalSection(&threadListCS);
-            printf("Thread ID(unutar main funkcije servera): %d\n", tParameters.threadId);
-            threadNode* tn = createNewThreadNode(thread, tParameters.threadId);
-            insertAtHead(&head, tn);
-            printList(head);
-            LeaveCriticalSection(&threadListCS);
-
-            
+            EnterCriticalSection(&(ltParams->threadListCS));
+            printf("Thread ID(unutar listen thread-a funkcije servera, id thread-a za komunikaciju sa klijentom): %d\n", tParameters->threadId);
+            threadNode* tn = createNewThreadNode(clientCommunicaitonThread, tParameters->threadId);
+            insertAtHead(ltParams->head, tn);
+            printList(*(ltParams->head));
+            LeaveCriticalSection(&(ltParams->threadListCS));
 
             /*
             // postavljanje soketa u neblokirajuci rezim
@@ -304,43 +400,14 @@ int  main(void)
                     closesocket(acceptedSocket);
                 }
             } while (iResult > 0);
-            
+
             // here is where server shutdown loguc could be placed*/
 
         }
 
-    //gde se vrsi logika zatvaranja soketa i brisanja threadova iz liste
+        //gde se vrsi logika zatvaranja soketa i brisanja threadova iz liste
 
     } while (1);
 
-    // shutdown the connection since we're done
-    iResult = shutdown(acceptedSocket, SD_SEND);
-    if (iResult == SOCKET_ERROR)
-    {
-        printf("shutdown failed with error: %d\n", WSAGetLastError());
-        closesocket(acceptedSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    // cleanup
-    closesocket(listenSocket);
-    closesocket(acceptedSocket);
-    WSACleanup();
-
-    //zatvaramo threadove
-
     return 0;
-}
-
-bool InitializeWindowsSockets()
-{
-    WSADATA wsaData;
-    // Initialize windows sockets library for this process
-    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-    {
-        printf("WSAStartup failed with error: %d\n", WSAGetLastError());
-        return false;
-    }
-    return true;
 }
